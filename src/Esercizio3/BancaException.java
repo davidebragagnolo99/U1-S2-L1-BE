2652da7f6515cfc14dc050b892428ef921fcbc15
davide bragagnolo <davidebraga.99@gmail.com>
2023-10-09 16:57:15 +0200

    ex3

---
+++ src/Esercizio3/BancaException.java
@@ -0,0 +1,17 @@
+package src.Esercizio3;
+
+public class BancaException extends Exception {
+
+    private String message;
+
+    public BancaException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+
+}
